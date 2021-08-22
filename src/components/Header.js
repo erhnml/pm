f83bd8e5cf47e5269446b6f68c5121227c02b678
Erhan Namal <namallerhan@gmail.com>
2021-08-22 20:16:19 +0300

    header component added

---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
+
+import TopBar from './TopBar';
+import Hamburger from '../assets/icons/hamburger.png';
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <Container>
+        <TextWrapper>
+          <MenuIcon src={Hamburger} alt="menu icon" width={20} height={20} />
+          <h3>ErhanNamal</h3>
+        </TextWrapper>
+        <TopBar />
+      </Container>
+    </HeaderWrapper>
+  );
+}
+
+const HeaderWrapper = styled.div`
+  background-color: #ff5e58;
+`;
+
+const TextWrapper = styled.div`
+  position: relative;
+  display: flex;
+  padding: 15px 0;
+  align-items: center;
+  justify-content: center;
+  h3 {
+    margin: 0;
+    font-size: 18px;
+    color: white;
+    font-weight: bold;
+  }
+`;
+
+const MenuIcon  = styled.img`
+  position: absolute;
+  left: 0;
+  cursor: pointer;
+  @media (min-width: 768px) { 
+    display: none;;
+  }
+`;
+
+export default Header;
